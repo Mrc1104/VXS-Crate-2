d0ddc264ae3989f2a5cb4ee65b7e13daedad92db
mrcconaway <mc321015@ohio.edu>
2023-06-16 13:27:02 -0400

    added header guard and basic fadc hit struct

---
+++ vxs.h
@@ -0,0 +1,25 @@
+#ifndef VXS_H
+#define VXS_H
+
+
+#include <ap_int.h>
+#include <hls_stream.h>
+
+// hit_t:
+// - every 32ns each fadc reports 13 bit energy, and 3 bit hit time (time offset in current 32ns clock: 0=0ns, 1=4ns, 2=8ns, ..., 7=28ns)
+// - if the channel has no hit, then the energy, e, will be reported as 0
+// - energy, e, will saturate at 8191 (e.g. if the FADC intergal [after pedastal subtr. and gian] is greater than 8191, the FADC reports 8191)
+typedef struct 
+{
+	ap_uint<13> e;
+	ap_uint<3> t;
+} hit_t;
+
+
+
+
+
+
+
+
+#endif
